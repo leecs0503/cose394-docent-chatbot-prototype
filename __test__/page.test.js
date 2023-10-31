581e97ef1768cb2ff2e98fca333c8475b53ba5da
Lee Changsu <leecs0503@naver.com>
2023-11-01 00:10:40 +0900

    test: 테스트 템플릿 작성

---
+++ __test__/page.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import Page from '@/page'
+import '@testing-library/jest-dom'
+
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Page />);
+
+    const heading = screen.getByRole('heading', {
+      name: /Hello, Next.js!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
