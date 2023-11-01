865dc020fac5fa0b8b36101a9c1f92942f100b54
lee changsu <leecs0503@naver.com>
2023-11-01 14:19:33 +0900

    feat: StreamOpenAIModel 구현

---
+++ chatbot/model/openAiModel.tsx
@@ -0,0 +1,29 @@
+import {Model} from "."
+import {OpenAI} from "openai"
+
+
+interface QueryRow {
+  role: 'system' | 'user' | 'assistant';
+  content: string
+}
+
+export class StreamOpenAIModel implements Model{
+  private openai: OpenAI
+  constructor(
+    apiKey: string,
+    private modelVersion: string,
+  ) {
+    this.openai = new OpenAI({apiKey})
+  }
+
+  async *predict(input: QueryRow[]) {
+    const stream = await this.openai.chat.completions.create({
+      model: this.modelVersion,
+      messages: input,
+      stream: true,
+    });
+    for await (const part of stream) {
+      yield part.choices[0]?.delta?.content || '';
+    }
+  }
+}
