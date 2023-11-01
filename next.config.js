d4df432833f9796b0a915f9694f0152ee4712ac2
lee changsu <leecs0503@naver.com>
2023-11-01 16:32:18 +0900

    refactor: lib을 global context에 해당될 폴더로 구성

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   eslint: {
-    dirs: ['pages', 'components', 'lib', 'app', 'utils', 'chatbot'], // 여기에 린팅하려는 폴더를 추가
+    dirs: ['pages', 'components', 'lib', 'app', 'utils', 'lib']
   },
 };
