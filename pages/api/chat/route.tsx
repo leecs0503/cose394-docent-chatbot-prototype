31abaa65a9bc2a7b3f057c5d180dc9efcc230304
lee changsu <leecs0503@naver.com>
2023-11-06 13:36:28 +0900

    refactor: 리뷰사항 수정1

--- pages/api/chat/route.tsx
+++ pages/api/chat/route.tsx
@@ -1,19 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const generators = {};
-import {openAiChatBot} from "../../lib/chatbot";
-import {ChatBotContext} from "../../lib/chatbot/chatBot";
+import {openAiChatBot} from "../../../lib/chatbot";
+import {ChatBotContext} from "../../../lib/chatbot/chatBot";
 
-export default async function handler(
+export async function POST(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const { requestid: requestid } = req.query;
-  if (req.method != "POST") {
-    // FIXME: nextjs에서 데코레이터 등으로 처리할 수 있는 기능 찾기
-    res.status(405);
-    return;
-  }
   // XXX: 적절한 validation으로 변환
   if (typeof requestid != "string"){
     return res.status(400).json("`requestid`should exist in query and type of requestid should be string.");
