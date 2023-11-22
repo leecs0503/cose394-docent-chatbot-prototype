b6912641d746bd52c1477c760d3c7baea6802839
r4bb1t <r4bb1t.github@gmail.com>
2023-11-22 19:54:40 +0900

    init: set up tailwind css & daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    theme: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
