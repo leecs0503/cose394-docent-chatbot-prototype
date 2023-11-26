a48f5ac92bfb9e5effdf7844c82747c38e4f10e3
Lee Changsu <leecs0503@naver.com>
2023-11-26 21:42:14 +0900

    feat: create table 스크립트 작성

--- script/create_table_query.sql
+++ script/create_table_query.sql
@@ -22,10 +22,3 @@ CREATE TABLE path_point (
     x INT                      NOT NULL,
     y INT                      NOT NULL
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO path (id, name) VALUES (0, "추천루트01");
-INSERT INTO path_point (ID, x, y, path_id) VALUES (0, 140, 406, 0), (1, 487, 82, 0);
-INSERT INTO place (ID, name) VALUES (0, "고려대박물관");
-INSERT INTO art_work (ID, name, summary, description) VALUES (0, "세모", "세모입니다", "검정색으로 구성된 세모는 …..."), (1, "네모", "네모입니다", "검정색으로 구성된 네모는 …...");
-INSERT INTO path (id, name) VALUES (0, "추천루트01");
-INSERT INTO path_point (ID, x, y, path_id) VALUES (0, 140, 406, 0), (1, 487, 82, 0);
