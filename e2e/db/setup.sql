eab37ff2f93e9dc6f1138c27fb95870fa152f0e2
Lee Changsu <leecs0503@naver.com>
2023-11-26 22:27:58 +0900

    fix: path에 place_id 추가

--- e2e/db/setup.sql
+++ e2e/db/setup.sql
@@ -12,12 +12,14 @@ CREATE TABLE art_work (
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE path (
-    id   INT          PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    id       INT          NOT NULL,
+    place_id INT          NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id, place_id)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE path_point (
-    id      INT,
+    id      INT NOT NULL,
     path_id INT NOT NULL,
     x       INT NOT NULL,
     y       INT NOT NULL,
@@ -30,17 +32,17 @@ SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
 
 -- place 쿼리:
 INSERT INTO place (id, name)
-       VALUES (0, "고려대박물관");
+       VALUES (1, "고려대박물관");
 
 -- artwork 쿼리:
 INSERT INTO art_work (id, name, summary, description, place_id)
-       VALUES (0, "세모", "세모입니다", "검정색으로 구성된 세모는 …...", 0),
-              (1, "네모", "네모입니다", "검정색으로 구성된 네모는 …...", 0);
+       VALUES (0, "세모", "세모입니다", "검정색으로 구성된 세모는 …...", 1),
+              (1, "네모", "네모입니다", "검정색으로 구성된 네모는 …...", 1);
 
 -- path 관련 쿼리:
--- pathID: 0 (파일: 추천루트01.xlsx)
-INSERT INTO path (id, name)
-       VALUES (0, "추천루트01");
+-- pathID: 1 (파일: 추천루트01.xlsx)
+INSERT INTO path (id, place_id, name)
+       VALUES (1, 1, "추천루트01");
 INSERT INTO path_point (id, x, y, path_id)
-       VALUES (0, 140, 406, 0),
-              (1, 487, 82, 0);
+       VALUES (0, 140, 406, 1),
+              (1, 487, 82, 1);
