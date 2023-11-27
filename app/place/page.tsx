d81b591a881c77d4d321a7bc4944239654886ad6
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2023-11-27 23:16:14 +0900

    feat: /place 구현

--- app/place/page.tsx
+++ app/place/page.tsx
@@ -1,3 +1,38 @@
+import { LucideIcon, MapPin, Pyramid } from "lucide-react";
+
+interface CardProps {
+  name: string;
+  id: string;
+  Icon: LucideIcon;
+}
+
+function Card({ name, id, Icon }: CardProps) {
+  return (
+    <div className="bg-base-100 rounded-xl relative mt-10 shadow-lg">
+      <div className="rounded-full w-20 flex items-center justify-center aspect-square bg-base-100 shadow-lg -translate-x-1/2 -translate-y-1/2 left-1/2 text-primary mx-auto absolute">
+        <Icon size={48} />
+      </div>
+      <div className="flex flex-col p-4 pt-14 gap-4">
+        <div className="text-xl font-bold text-primary text-center">{name}</div>
+        <a href={`/place/${id}`} className="btn btn-primary w-64">
+          가이드 시작하기
+        </a>
+      </div>
+    </div>
+  );
+}
+
 export default function Place() {
-  return <div>/place (캠투 할지 관람 할지 고르셈)</div>;
+  return (
+    <div className="bg-primary min-h-[100dvh] flex justify-center">
+      <ul className="flex flex-col gap-8 items-center my-auto">
+        <li>
+          <Card name="고려대학교 박물관" id="museum" Icon={MapPin} />
+        </li>
+        <li>
+          <Card name="고려대학교 캠퍼스 투어" id="campus" Icon={Pyramid} />
+        </li>
+      </ul>
+    </div>
+  );
 }
