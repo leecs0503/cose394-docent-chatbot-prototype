3a59a13d42959fa87772d9a11a332d398f619488
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2023-11-27 22:24:47 +0900

    feat: 데스크톱 환경 고려하여 전역 max-width 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-<<<<<<< HEAD
       <body className="relative max-w-2xl mx-auto min-h-[100dvh]">
         {children}
       </body>
-=======
-      <body>{children}</body>
->>>>>>> 0f5f825 (init: Tailwind CSS 작동에 필요한 의존성 설치 및 globals.css 작성)
     </html>
   );
 }
