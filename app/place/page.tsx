314e0a68944580fc517cc057658c8d44154f4938
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2023-11-29 16:17:33 +0900

    refactor: API 응답 임시로 상수 처리

--- app/place/page.tsx
+++ app/place/page.tsx
@@ -23,15 +23,28 @@ function Card({ name, id, Icon }: CardProps) {
 }
 
 export default function Place() {
+  // TODO: fetch places from server
+  const PLACES = [
+    {
+      name: "고려대학교 박물관",
+      id: "museum",
+      Icon: MapPin,
+    },
+    {
+      name: "고려대학교 캠퍼스 투어",
+      id: "campus",
+      Icon: Pyramid,
+    },
+  ];
+
   return (
     <div className="bg-primary min-h-[100dvh] flex justify-center">
       <ul className="flex flex-col gap-8 items-center my-auto">
-        <li>
-          <Card name="고려대학교 박물관" id="museum" Icon={MapPin} />
-        </li>
-        <li>
-          <Card name="고려대학교 캠퍼스 투어" id="campus" Icon={Pyramid} />
-        </li>
+        {PLACES.map((place) => (
+          <li key={place.id}>
+            <Card {...place} />
+          </li>
+        ))}
       </ul>
     </div>
   );
