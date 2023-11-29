1e041a310e2e679c43587e89e037113b251e69c5
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2023-11-29 10:50:34 +0900

    feat: /place/:placeId 퍼블리싱

--- app/place/[placeId]/page.tsx
+++ app/place/[placeId]/page.tsx
@@ -1,12 +1,49 @@
+interface PlaceDetailProps {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+}
+
+function PlaceDetail({
+  id,
+  name,
+  description,
+  image,
+  imageAlt,
+}: PlaceDetailProps) {
+  return (
+    <div>
+      <img className="aspect-video bg-stone-300" src={image} alt={imageAlt} />
+      <div className="flex flex-col gap-3 p-6 pb-24 break-keep">
+        <h1 className="font-bold text-2xl whitespace-pre-wrap">{name}</h1>
+        <p className="text-stone-500">{description}</p>
+      </div>
+      <div className="absolute bottom-0 right-0 left-0 w-full p-6 pt-0">
+        <a
+          href={`/place/${id}/path`}
+          className="btn btn-primary w-full shadow-md shadow-primary/30"
+        >
+          관람 루트 선택하기
+        </a>
+      </div>
+    </div>
+  );
+}
+
 export default function Place({
   params: { placeId },
 }: {
   params: { placeId: string };
 }) {
   return (
-    <div>
-      /place/:placeId
-      <button>관람 루트 선택하기</button>
-    </div>
+    <PlaceDetail
+      id={placeId}
+      name={placeId}
+      description={placeId}
+      image={""}
+      imageAlt={""}
+    />
   );
 }
