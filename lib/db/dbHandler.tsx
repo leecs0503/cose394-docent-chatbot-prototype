904233f235fab22a64618b7bf78efb1dd9cf70cc
Lee Changsu <leecs0503@naver.com>
2023-11-29 23:03:43 +0900

    fix: mysql에 맞게 동작을 수정

--- lib/db/dbHandler.tsx
+++ lib/db/dbHandler.tsx
@@ -5,29 +5,29 @@ import {BaseDB} from "./db";
 export class DBHandler {
     constructor(private db: BaseDB) {}
     async getPlaces(): Promise<Place[]> {
-        const sql = "SELECT (id, name, description) FROM place;";
+        const sql = "SELECT id, name, description FROM place;";
         const result = await this.db.get(sql, []);
-        return result.map(([id, name, description]) => ({id, name, description}));
+        return result;
     }
 
     async getArtWorks(placeId: number): Promise<ArtWork[]> {
-        const sql = "SELECT (id, placeId, name, summary, description) FROM artwork WHERE place_id = ?;";
+        const sql = "SELECT id, placeId, name, summary, description FROM artwork WHERE place_id = ?;";
         const args = [placeId];
         const result = await this.db.get(sql, args);
-        return result.map(([id, placeId, name, summary, description])=>({id, placeId, name, summary, description}));
+        return result;
     }
 
     async getPaths(placeId: number): Promise<Path[]> {
-        const sql = "SELECT (id, placeId, name) FROM path WHERE place_id = ?;";
+        const sql = "SELECT id, placeId, name FROM path WHERE place_id = ?;";
         const args = [placeId];
         const result = await this.db.get(sql, args);
-        return result.map(([id, placeId, name])=>({id, placeId, name}));
+        return result;
     }
 
     async getPathPoints(pathId: number): Promise<PathPoint[]> {
-        const sql = "SELECT (id, placeId, x, y) FROM path_point WHERE place_id = ?;";
+        const sql = "SELECT id, placeId, x, y FROM path_point WHERE place_id = ?;";
         const args = [pathId];
         const result = await this.db.get(sql, args);
-        return result.map(([id, placeId, x, y])=>({id, placeId, x, y}));
+        return result;
     }
 };
