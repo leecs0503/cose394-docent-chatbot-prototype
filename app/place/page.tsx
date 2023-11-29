cf0d4980940d6332db04b34274e8b9a9fa025596
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2023-11-29 22:52:45 +0900

    refactor: use imported types

--- app/place/page.tsx
+++ app/place/page.tsx
@@ -1,20 +1,33 @@
-import { LucideIcon, MapPin, Pyramid } from "lucide-react";
+import { MapPin, Pyramid } from "lucide-react";
+
+import { Place } from "../../lib/interfaces";
 
 interface CardProps {
-  name: string;
-  id: string;
-  Icon: LucideIcon;
+  place: Place;
 }
 
-function Card({ name, id, Icon }: CardProps) {
+function Card({ place }: CardProps) {
+  const Icon = (function () {
+    switch (place.name) {
+      case "고려대학교 박물관":
+        return MapPin;
+      case "고려대학교 캠퍼스 투어":
+        return Pyramid;
+      default:
+        return MapPin;
+    }
+  })();
+
   return (
     <div className="bg-base-100 rounded-xl relative mt-10 shadow-lg">
       <div className="rounded-full w-20 flex items-center justify-center aspect-square bg-base-100 shadow-lg -translate-x-1/2 -translate-y-1/2 left-1/2 text-primary mx-auto absolute">
         <Icon size={48} />
       </div>
       <div className="flex flex-col p-4 pt-14 gap-4">
-        <div className="text-xl font-bold text-primary text-center">{name}</div>
-        <a href={`/place/${id}`} className="btn btn-primary w-64">
+        <div className="text-xl font-bold text-primary text-center">
+          {place.name}
+        </div>
+        <a href={`/place/${place.id}`} className="btn btn-primary w-64">
           가이드 시작하기
         </a>
       </div>
@@ -22,18 +35,19 @@ function Card({ name, id, Icon }: CardProps) {
   );
 }
 
-export default function Place() {
+export default function PlacePage() {
   // TODO: fetch places from server
-  const PLACES = [
+  const PLACES: Place[] = [
     {
+      id: 0,
       name: "고려대학교 박물관",
-      id: "museum",
-      Icon: MapPin,
+      description:
+        "고려대학교 박물관은 고려대학교의 역사를 보여주는 박물관입니다.",
     },
     {
+      id: 1,
       name: "고려대학교 캠퍼스 투어",
-      id: "campus",
-      Icon: Pyramid,
+      description: "고려대학교의 캠퍼스를 돌아보는 코스입니다.",
     },
   ];
 
@@ -42,7 +56,7 @@ export default function Place() {
       <ul className="flex flex-col gap-8 items-center my-auto">
         {PLACES.map((place) => (
           <li key={place.id}>
-            <Card {...place} />
+            <Card place={place} />
           </li>
         ))}
       </ul>
