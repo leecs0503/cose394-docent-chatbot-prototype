de14943cbd8b24f622618368ed3f2220d9f99154
r4bb1t <r4bb1t.github@gmail.com>
2023-12-03 20:17:18 +0900

    feat: 메인화면 클릭 시 온보드로

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>/home</div>;
+  return (
+    <Link
+      href={"/onboard"}
+      className="w-full h-screen font-[museum] bg-dark flex items-center justify-center flex-col text-white"
+    >
+      <div className="text-[15px] font-light">내 손 안의 도슨트,</div>
+      <div className="font-bold text-[50px]">DoSonT</div>
+    </Link>
+  );
 };
 
 export default Home;
