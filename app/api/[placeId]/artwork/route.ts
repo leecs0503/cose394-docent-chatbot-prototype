891a670e1d146db20bcf8b891feb12c4f2a72647
Lee Changsu <leecs0503@naver.com>
2023-12-03 19:17:34 +0900

    feat: api들이 동작하도록 수정

--- app/api/[placeId]/artwork/route.ts
+++ app/api/[placeId]/artwork/route.ts
@@ -3,10 +3,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import { dbHandler } from '../../../../lib/db';
 
 export async function GET(req: NextRequest, {params}) {
-    // TODO: implement
     const {placeId} = params;
-    const result = await dbHandler.getPaths(placeId);
 
+    const result = await dbHandler.getArtWorks(placeId);
     return NextResponse.json(result, {
         status: 200
     });
--- app/api/[placeId]/path/[pathId]/route.ts
+++ app/api/[placeId]/path/[pathId]/route.ts
@@ -1,23 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function GET(req: NextRequest, {params}) {
+import { dbHandler } from '../../../../../lib/db';
+
+export async function GET(req: NextRequest, {params}) {
     // TODO: implement
     const {pathId} = params;
+    const result = await dbHandler.getPathPoints(pathId);
 
-    const result = [
-        {
-            id: 0,
-            pathId: pathId,
-            x: 140,
-            y: 406,
-        },
-        {
-            id: 1,
-            pathId: pathId,
-            x: 487,
-            y: 82,
-        },
-    ];
     return NextResponse.json(result, {
         status: 200
     });
