faf3ed00de26e16ea06376f2064197f06d73f880
Lee Changsu <leecs0503@naver.com>
2023-12-03 19:17:34 +0900

    feat: api들이 동작하도록 수정

--- app/api/place/route.ts
+++ app/api/place/route.ts
@@ -1,12 +1,9 @@
 import { NextResponse } from 'next/server';
 
-export function GET() {
-    const result = [
-        {
-            id: 1,
-            name: "고려대박물관",
-        }
-    ];
+import { dbHandler } from '../../../lib/db';
+
+export async function GET() {
+    const result = await dbHandler.getPlaces();
     return NextResponse.json(result, {
         status: 200
     });
