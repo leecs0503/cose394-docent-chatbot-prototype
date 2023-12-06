7f8b6862a13608f05764c2ae46607278e6e5dde3
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2023-12-06 17:37:51 +0900

    feat: add umami script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,6 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <Script
+          async
+          src="https://us.umami.is/script.js"
+          data-website-id="c6aeb1e1-7da3-4549-874e-b36b1d0063d7"
+        />
+      </head>
       <body className="relative max-w-2xl mx-auto min-h-[100dvh]">
         {children}
       </body>
