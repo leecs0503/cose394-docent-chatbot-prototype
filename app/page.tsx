21984ab00bf980e4f1946d67705f6f3d237428d5
lee changsu <leecs0503@naver.com>
2023-12-06 14:40:52 +0900

    feat: onboard 디스크립션 및 임시 이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,17 @@ interface StepProps {
 function Step({ title, description, image, imageAlt, onNextClick }: StepProps) {
   return (
     <div>
-      <img className="aspect-square bg-stone-300" src={image} alt={imageAlt} />
+      <div className="flex justify-center">
+        <img className="aspect-square bg-stone-300" src={image} alt={imageAlt} />
+      </div>
       <div className="flex flex-col gap-3 p-6 pb-28 break-keep">
         <h1 className="font-bold text-2xl whitespace-pre-wrap">{title}</h1>
-        <p className="text-stone-500">{description}</p>
+        <p className="text-stone-500">
+          {
+            (!description.includes("\n")) ? description
+            : description.split("\n").map((v) => <div>{v}</div>)
+          }
+        </p>
       </div>
       <button
         onClick={onNextClick}
@@ -34,21 +41,23 @@ export default function OnBoard() {
   const ONBOARDING_STEPS = [
     {
       title: "도손트는 누구에게나 ’손쉬운’\n관광 문화를 만듭니다.",
-      description: "여기에 설명 작성",
-      image: "",
+      description: "여러분들만의 개인 온라인 관광 가이드 '도손트'를 소개합니다.",
+      image: "/images/onboard/1.png",
       imageAlt: "",
     },
     {
-      title: "해당 MVP 서비스 소개",
-      description: "여기에 설명 작성",
-      image: "",
+      title: "해당 서비스 소개",
+      description: "여러분들은 '도손트'를 통해 관광지 탐색, 관광지 추천 루트와 가이드를 확인할 수 있습니다.",
+      image: "/images/onboard/2.png",
       imageAlt: "",
     },
     {
       title: "도손트 서비스 소개를 위한\n튜토리얼",
       description:
-        "오디오 가이드가 자동으로 재생되니 꼭 이어폰을 착용하거나 음소거를 해제하고 가이드를 실행하시기 바랍니다.",
-      image: "",
+        "1. 여러분들이 가고자하는 관광지를 탐색할 수 있습니다. 관광하고자 하는 관광지를 선택해보세요.\n" +
+        "2. 여러분들이 원하는 관광 컨셉을 선택할 수 있습니다. 관광하고자 하는 추천루트를 선택해보세요.\n" +
+        "3. 지도의 핀을 선택하시면 가이드를 들을 수 있습니다. 가이드 화면의 각종 기능들을 확인해보세요.",
+      image: "/images/onboard/3.png",
       imageAlt: "",
     },
   ];
